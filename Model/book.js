0a2f472513828788f8ab79e0d82d988a17f29b01
Vishrant0281 <vrank988@rku.ac.in>
2021-01-28 17:03:10 +0530

    get request

---
+++ Model/book.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema({
+    name:String,
+    qty:Number
+});
+
+module.exports = mongoose.model("Book", schema);
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./route');
+const bodyParser = require('body-parser');
+
+//connection to mongodb
+mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+    () => {
+        const app = express();
+        app.use(bodyParser.urlencoded({extended:true}));
+        app.use(express.json());
+        app.use("/api",route);
+        app.listen(3000,() =>{
+            console.log("server started..!!");
+        })
+    }
+);
