db768edbcb16037aa19cde39b08aff30ef5f70ab
Vishrant0281 <vrank988@rku.ac.in>
2021-01-28 17:10:13 +0530

    db online

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const route = require('./route');
 const bodyParser = require('body-parser');
 
 //connection to mongodb
-mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://vrank:Vrank_2001@cluster0.5hxvx.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     () => {
         const app = express();
         app.use(bodyParser.urlencoded({extended:true}));
