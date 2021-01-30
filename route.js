9b34288e752492191a010b67c6b375bbc37a920c
Vishrant0281 <vrank988@rku.ac.in>
2021-01-30 16:37:07 +0530

    CURD

--- route.js
+++ route.js
@@ -9,4 +9,35 @@ router.get("/books", async (req,res)=>{
 
 });
 
+router.post('/books', async (req, res) => {
+    const book = new Book({
+        name: req.body.name,
+        qty: req.body.qty
+    });
+    await book.save();
+    res.send(book);
+});
+
+router.patch("/books/:id", async (req, res) => {
+    try {
+        const book = await Book.findOne({ _id: req.params.id });
+        book.name = req.body.name;
+        book.qty = req.body.qty;
+        await book.save();
+        res.send(book);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+router.delete("/books/:id", async (req, res) => {
+    try {
+        await Book.deleteOne({ _id: req.params.id });
+        res.status(204);
+        res.send("deleted");
+    } catch {
+        res.status(404);
+        res.send({ error: "Post doesn't exist!" });
+    }
+});
 module.exports = router;
